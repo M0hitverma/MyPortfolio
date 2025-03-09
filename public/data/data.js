797f86513e1f1be1ab57e41e59be76b174e2d64e
Mohit <91580016+M0hitverma@users.noreply.github.com>
2025-03-09 13:00:28 +0530

    Update data.js

--- public/data/data.js
+++ public/data/data.js
@@ -1,4 +1,4 @@
-import dp from "../assets/dp.JPG";
+import dp from "../assets/dp.jpg";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
 import { IoIosDocument } from "react-icons/io";
